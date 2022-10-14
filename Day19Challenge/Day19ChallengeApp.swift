f470f3f623ee19b80d601e4219744bae232e77a9
Matilda <afemalecoder@gmail.com>
2022-10-14 09:59:05 +0200

    Initial Commit

---
+++ Day19Challenge/Day19ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Day19ChallengeApp.swift
+//  Day19Challenge
+//
+//  Created by Matilda Cederberg on 14/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Day19ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
