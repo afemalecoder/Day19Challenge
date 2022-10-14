4c706161e0b480947cb2a17557bf8d022e5bb4ed
Matilda <afemalecoder@gmail.com>
2022-10-14 10:00:42 +0200

    first commit

--- Day19Challenge/ContentView.swift
+++ Day19Challenge/ContentView.swift
@@ -2,20 +2,85 @@
 //  ContentView.swift
 //  Day19Challenge
 //
-//  Created by Matilda Cederberg on 14/10/2022.
+//  Created by Matilda Cederberg on 13/10/2022.
 //
+
 
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+    
+    enum TempType: String, CaseIterable{
+        case celsius = "Celsius"
+        case fahrenheit = "Faharenheit"
+        case kelvin = "Kelvin"
+    }
+    
+    @State private var inputUnit: TempType = .celsius
+    @State private var outputUnit: TempType = .celsius
+    @State private var firstAmount = ""
+    
+    var conversion: Double {
+        guard let inputAmount = Double(firstAmount) else { return 0 }
+        var conversionInput = 0.0
+        var conversionResult = 0.0
+        
+        switch inputUnit{
+        case .celsius:
+            conversionInput = inputAmount + 273.15
+        case .fahrenheit:
+            conversionInput = (inputAmount - 32) * 5 / 9 + 273.15
+        case .kelvin:
+            conversionInput = inputAmount
         }
-        .padding()
+        switch outputUnit{
+        case .celsius:
+            conversionResult = conversionInput - 273.15
+        case .fahrenheit:
+            conversionResult = ((conversionInput - 273.15) * 9 / 5) + 32
+        case .kelvin:
+            conversionResult = conversionInput
+        }
+        return conversionResult
+    }
+    
+    var body: some View {
+        NavigationView{
+            Form {
+                Section{
+                    TextField("Amount", text: $firstAmount)
+                }header: {
+                    Text("Convert")
+                }
+                
+                Section{
+                    Picker("Select Unit", selection: $inputUnit){
+                        ForEach(TempType.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("Select input unit")
+                }
+                
+                Section{
+                    Picker("Select Unit", selection: $outputUnit){
+                        ForEach(TempType.allCases, id: \.self){
+                            Text($0.rawValue)
+                        }
+                    }.pickerStyle(.segmented)
+                }header: {
+                    Text("Select output unit")
+                }
+                
+                Section{
+                    Text("\(conversion, specifier: "%.2F")")
+                }header: {
+                    Text("Converted")
+                }
+            }
+            .navigationTitle("Converter")
+        }
     }
 }
 
